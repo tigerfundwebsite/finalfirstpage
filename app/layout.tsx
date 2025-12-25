5e2a35cd25a347c73eeceb1a4d2d0b3e90e29b74
tigerfundwebsite <tigerfundwebsite@gmail.com>
2025-12-25 15:40:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,6 @@ export const metadata: Metadata = {
   title: "Tiger Assets Pvt Ltd - Portfolio Management Services",
   description:
     "Investing in Emerging Leaders Across India's Growth Sectors. Equity-focused investment firm with deep domain knowledge and proven track record.",
-  generator: "v0.app",
   icons: {
     icon: "/orange-tiger-icon.svg",
     apple: "/orange-tiger-icon.svg",
@@ -21,41 +20,33 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google Analytics (GA4) */}
         <Script
-          id="gtm-head"
+          src="https://www.googletagmanager.com/gtag/js?id=G-L58E4L4FE4"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga4-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TL5TN3PL');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-L58E4L4FE4');
             `,
           }}
         />
-        {/* End Google Tag Manager */}
+        {/* End Google Analytics */}
       </head>
 
       <body className="font-sans antialiased">
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TL5TN3PL"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
         {children}
         <Analytics />
       </body>
